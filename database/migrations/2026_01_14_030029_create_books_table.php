cefb4c0b6f8cb73949de37e1b3b6b5aa9bcd58bb
Bagus <bagusardiansah981@gmail.com>
2026-03-09 10:49:51 +0700

    fix migration transactions

--- database/migrations/2026_01_14_030029_create_books_table.php
+++ database/migrations/2026_01_14_030029_create_books_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('kode_buku')->unique();
             $table->string('judul');
             $table->string('pengarang');
             $table->year('tahun_terbit');
